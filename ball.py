b5d846256a9fe09130a34f947a79f301ae5be935
Marco <fernstaedtmarco@gmail.com>
2022-10-09 13:50:00 -0700

    built attribute for movment speed and increase the spead when it contacts a paddle and reset speed when ball resets

--- ball.py
+++ ball.py
@@ -9,13 +9,17 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def bounce_y(self):
         self.y_move *= -1
@@ -23,3 +27,4 @@ class Ball(Turtle):
     def reset_position(self):
         self.goto(0, 0)
         self.bounce_x()
+        self.move_speed = 0.1
