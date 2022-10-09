2f389b48f5602ed599b748fa285a1c2326e436dd
Marco <fernstaedtmarco@gmail.com>
2022-10-08 22:06:22 -0700

    built a bounce function for x and y when they hit a wall or paddle

--- ball.py
+++ ball.py
@@ -14,6 +14,8 @@ class Ball(Turtle):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+    def bounce_x(self):
+        self.x_move *= -1
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
