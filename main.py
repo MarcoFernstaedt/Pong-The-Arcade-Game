54594501219819eb1fa2309a3f2a668df06f3b5f
Marco <fernstaedtmarco@gmail.com>
2022-10-08 21:01:48 -0700

    imported paddle and created nstances of the object for right paddle and left paddle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from turtle import Screen, Turtle, width
-
-from numpy import pad
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(800, 600)
@@ -8,9 +7,16 @@ screen.bgcolor('black')
 screen.title('Pong')
 screen.tracer(0)
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
 screen.listen()
-screen.onkey(go_up, 'Up')
-screen.onkey(go_down, 'Down')
+# Player 1 controls
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+# Player 2 controls
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
